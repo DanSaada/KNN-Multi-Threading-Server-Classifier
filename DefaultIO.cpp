2305e5039eb27bccd92cb006b8bc9afc7c744b1d
DanSaada <dansaada2@gmail.com>
2023-01-11 17:54:51 +0200

    creating DefaultIO

---
+++ DefaultIO.cpp
@@ -0,0 +1,13 @@
+//
+// Created by dansa97 on 1/11/23.
+//
+
+#include "DefaultIO.h"
+
+string DefaultIO::read() {
+    return std::string();
+}
+
+void DefaultIO::write(string output) {
+
+}
---
+++ DefaultIO.h
@@ -0,0 +1,18 @@
+//
+// Created by dansa97 on 1/11/23.
+//
+
+#ifndef KNN_SERVER_DEFAULTIO_H
+#define KNN_SERVER_DEFAULTIO_H
+
+using namespace std;
+#include "string"
+
+class DefaultIO {
+string  read();
+void write(string output);
+
+};
+
+
+#endif //KNN_SERVER_DEFAULTIO_H
