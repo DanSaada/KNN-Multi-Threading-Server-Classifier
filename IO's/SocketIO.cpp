9f97d6d59786b565888414609b360213c7c4eca4
DanSaada <dansaada2@gmail.com>
2023-01-17 20:38:45 +0200

    implement SocketIO

--- IO's/SocketIO.cpp
+++ IO's/SocketIO.cpp
@@ -6,17 +6,30 @@
 
 
 /**
-* This function read a string from the user.
+* This function read a string from the client using socket.
 * @return a string from the user
 */
-string  SocketIO:: read() {
+string SocketIO:: read() {
+    char temp = 0;
+    string data;
 
+    //read data from the client char by char and create a string from it
+    while (temp != '\n') {
+        recv(this->clientID, &temp, sizeof(char), 0);
+
+        //end of data
+        if(temp == '\n'){
+            break;
+        }
+        data += temp;
+    }
+    return data;
 };
 
 /**
-* This function write a string to the user.
+* This function write a string to the client using socket.
 * @param text
 */
 void SocketIO:: write(string text) {
-
+    send(this->clientID, text.c_str(), text.length(), 0);
 };
--- IO's/SocketIO.h
+++ IO's/SocketIO.h
@@ -5,10 +5,21 @@
 #ifndef KNN_SERVER_SOCKETIO_H
 #define KNN_SERVER_SOCKETIO_H
 #include "DefaultIO.h"
+#include "iostream"
+#include <sys/socket.h>
+#include <stdio.h>
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "unistd.h"
 
 class SocketIO : public DefaultIO{
+private:
+    int clientID;
 public:
+    explicit SocketIO(int id):clientID(id){}
+
     string  read() override;
+
     void write(string output) override;
 };
 
